b1f7ea0d13331d5c97881264475abd6268be9ac5
marko <markozoreski@gmail.com>
2024-02-18 21:26:35 +0100

    Added initial html pages

--- Zakazi/angularapp/src/app/app.module.ts
+++ Zakazi/angularapp/src/app/app.module.ts
@@ -8,9 +8,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { API_BASE_URL, WebApiClient } from './services/web-api-client.service';
 import { environment } from './environments/environment';
 import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OffersComponent } from './offers/offers.component';
+import { EditOfferComponent } from './offers/edit-offer/edit-offer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
+  declarations: [AppComponent, HomePageComponent, LoginComponent, RegisterComponent, ProfileComponent, OffersComponent, EditOfferComponent],
   imports: [BrowserModule, HttpClientModule, NgbModule, SharedModule],
   providers: [
     WebApiClient,
--- Zakazi/angularapp/src/app/home-page/home-page.component.ts
+++ Zakazi/angularapp/src/app/home-page/home-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-
+  constructor(private router: Router) {}
+  onLoginClick() {
+    this.router.navigate(['/login']);
+  }
+  onRegisterClick() {
+    this.router.navigate(['/register']);
+  }
+  onAppointmentClick() {
+    this.router.navigate(['/offers']);
+  }
 }

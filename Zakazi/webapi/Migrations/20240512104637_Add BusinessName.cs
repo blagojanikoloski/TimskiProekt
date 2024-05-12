43e1997f5db859dfb53f09d411c4ba1df882398d
Blagoja Nikoloski <blagojanikoloski1@gmail.com>
2024-05-12 12:47:45 +0200

    Added businessName

---
+++ Zakazi/webapi/Migrations/20240512104637_Add BusinessName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBusinessName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BusinessName",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BusinessName",
+                table: "Businesses");
+        }
+    }
+}

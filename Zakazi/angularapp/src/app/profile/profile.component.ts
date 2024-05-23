8146627733f1f8c6b50c1eeafb21685dad0a9330
Blagoja Nikoloski <blagojanikoloski1@gmail.com>
2024-05-23 13:16:09 +0200

    Edit profile added

--- Zakazi/angularapp/src/app/profile/profile.component.ts
+++ Zakazi/angularapp/src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -9,21 +10,101 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+
+  profileData: any;
+  resultMessage = '';
+
+  editForm!: FormGroup;
+  isEditing: boolean = false;
+
+
   requestOptionsClientPending: any[] = [];
   requestOptionsWorker: any[] = [];
   postOptionsWorker: any[] = [];
-  constructor(private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService) {
   }
 
   ngOnInit(): void {
-    this.fetchAllRequestsClientPending();
-    this.fetchAllRequestsWorker();
-    this.fetchAllPostsWorker();
+    //this.fetchAllRequestsClientPending();
+    //this.fetchAllRequestsWorker();
+    //this.fetchAllPostsWorker();
+    this.getProfileData();
+    this.editForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+    });
   }
 
   editOffer(id : number) {
     this.router.navigate(["offers/edit", id]);
   }
+
+
+  getProfileData(): void {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const user = JSON.parse(userString);
+      const token = this.jwtHelper.decodeToken(user);
+      const userId: number = +token.nameid;
+
+      this.http.get<any>(`https://localhost:7200/api/Account/profile/${userId}`).subscribe(
+        (response) => {
+          this.profileData = response;
+          console.log('Profile Data:', this.profileData);
+
+          // Prefill the form with fetched profile data
+          this.editForm.patchValue({
+            name: this.profileData.name,
+            surname: this.profileData.surname,
+            email: this.profileData.email,
+            phoneNumber: this.profileData.phoneNumber
+          });
+        },
+        (error) => {
+          console.error('Error fetching profile data:', error);
+        }
+      );
+    }
+  }
+
+  submitEdit(): void {
+
+    if (this.editForm.valid) {
+
+      const userString = localStorage.getItem('user');
+      if (userString) {
+        const user = JSON.parse(userString);
+        const token = this.jwtHelper.decodeToken(user);
+        const userId: number = +token.nameid;
+
+        const editedProfile = this.editForm.value;
+        console.log('Payload:', editedProfile);
+
+        // Assuming you're using HttpClient from '@angular/common/http'
+        this.http.put(`https://localhost:7200/api/Account/edit-profile/${userId}`, editedProfile).subscribe(
+          () => {
+            console.log('Profile updated successfully');
+            // Optionally, you can reload the profile data after update
+            this.getProfileData();
+          },
+          error => {
+            console.error('Error updating profile:', error);
+          }
+        );
+      }
+    }
+  }
+
+  cancelEditing(): void {
+    this.isEditing = false;
+  }
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
+  }
+
 
   onDeleteOfferClick() {
     // Implement delete offer functionality if needed
---
+++ Zakazi/webapi/Domain/DTOs/EditProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Domain.DTOs
+{
+    public class EditProfileDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+
+}
